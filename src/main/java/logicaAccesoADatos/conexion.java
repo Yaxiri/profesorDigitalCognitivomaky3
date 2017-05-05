8eedbfeaaf92fd65a0fac2005a7061ab3cda999a
Yaxiri <yaxiri191@gmail.com>
2017-05-05 17:08:20 -0600

    logica de negocios
    conexion

--- src/main/java/logicaAccesoADatos/conexion.java
+++ src/main/java/logicaAccesoADatos/conexion.java
@@ -8,36 +8,105 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Vector;
 
-import com.mysql.jdbc.Statement;
-
-import logicaDeNegocios.Usuario;
+import logicaDeNegocios.Curso;
 
 public class conexion {
 
-	private static final String SQL_SERIALIZE_OBJECT = "INSERT INTO serialized_java_objects(object_name, serialized_object) VALUES (?, ?)";
-	private static final String SQL_DESERIALIZE_OBJECT = "SELECT serialized_object FROM serialized_java_objects WHERE object_name = ?";
-	
+	private static final String SQL_SERIALIZE_OBJECTUsuario = "INSERT INTO usuario(name, object_value) VALUES (?, ?)";
+	private static final String SQL_DESERIALIZE_OBJECTUsuario = "SELECT object_value FROM usuario WHERE name = ?";
 
-	public static Connection conexionDB() throws ClassNotFoundException, SQLException{
+	private static final String SQL_SERIALIZE_OBJECTCurso = "INSERT INTO curso(name, object_value) VALUES (?, ?)";
+	private static final String SQL_DESERIALIZE_OBJECTCurso = "SELECT object_value FROM curso WHERE name = ?";
+	private static final String SQL_DELETE_OBJECTCursos = "DELETE FROM curso WHERE name=?";
+
+	private static final String SQL_DESERIALIZE_OBJECTCursos = "SELECT name FROM curso";
+
+	private static final String SQL_CLOSE_CONNECTION = "select concat('KILL ',id,';') from information_schema.processlist where user='b63d126d987374';";
+
+	public Connection conexionDB() throws ClassNotFoundException, SQLException {
 		Connection connection = null;
 
 		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/ad_ed0a33a710978e7";
-		String username = "b6cf09759bc632";
-		String password = "984a8b90";
+		String url = "jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/ad_df5e3357b4ef02b";
+		String username = "b63d126d987374";
+		String password = "5dc317b4";
 		Class.forName(driver);
 		connection = DriverManager.getConnection(url, username, password);
 		return connection;
+
+	}
+
+	public ArrayList<String> obtenerIdCursos(Connection connection) {
+
+		ArrayList<String> cursosId = new ArrayList<String>();
+		Statement st;
+		try {
+			st = connection.createStatement();
+			ResultSet rs = st.executeQuery(SQL_DESERIALIZE_OBJECTCursos);
+			while (rs.next()) {
+				String name = rs.getString("name");
+				System.out.println(name);
+				cursosId.add(name);
+			}
+			rs.close();
+			connection.close();
+			return cursosId;
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cursosId;
+
+	}
+	
+	public void actualizarCurso(Connection connection,Curso curso){
+		eliminarCurso(connection,curso.getId());
+		try {
+			serializeJavaObjectToDBCurso(connection,curso.getId(),curso);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public static void serializeJavaObjectToDB(Connection connection,String nombre,Object objetoGuardar,String id) throws SQLException {
+	
+	public void eliminarCurso(Connection connection,String curso) {
+		cerrarConexiones(connection);
+		try {
+			PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_DELETE_OBJECTCursos);
+			ejecucionQuery.setString(1, curso);
+			ejecucionQuery.execute();
+			
+			ejecucionQuery.close();
+			connection.close();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+	}
 
+	@SuppressWarnings("unused")
+	public static void serializeJavaObjectToDB(Connection connection, String id, Object objetoGuardar)
+			throws SQLException {
+		cerrarConexiones(connection);
 		// just setting the class name
-		PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_SERIALIZE_OBJECT);
+		PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_SERIALIZE_OBJECTUsuario);
 
 		// just setting the class name
 		ejecucionQuery.setString(1, id);
@@ -61,52 +130,104 @@ public class conexion {
 	 * @throws IOException
 	 * @throws ClassNotFoundException
 	 */
-	public static Object deSerializeJavaObjectFromDB(Connection connection,String serialized_id){
-		try{
-		PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_DESERIALIZE_OBJECT);
-		ejecucionQuery.setString(1, serialized_id);
-		ResultSet resultadoQuery = ejecucionQuery.executeQuery();
-		resultadoQuery.next();
-			
-		byte[] buf = resultadoQuery.getBytes(1);
-		ObjectInputStream objectIn = null;
-		if (buf != null)
-			objectIn = new ObjectInputStream(new ByteArrayInputStream(buf));
-		Object deSerializedObject = objectIn.readObject();
+	public static Object deSerializeJavaObjectFromDB(Connection connection, String serialized_id) {
+		try {
+			cerrarConexiones(connection);
+			PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_DESERIALIZE_OBJECTUsuario);
+			ejecucionQuery.setString(1, serialized_id);
+			ResultSet resultadoQuery = ejecucionQuery.executeQuery();
+			resultadoQuery.next();
 
-		resultadoQuery.close();
-		ejecucionQuery.close();
-		connection.close();
-		
-		return deSerializedObject;
-		
-		}catch (Exception e) {
+			byte[] buf = resultadoQuery.getBytes(1);
+			ObjectInputStream objectIn = null;
+			if (buf != null)
+				objectIn = new ObjectInputStream(new ByteArrayInputStream(buf));
+			Object deSerializedObject = objectIn.readObject();
+
+			resultadoQuery.close();
+			ejecucionQuery.close();
+			connection.close();
+			cerrarConexiones(connection);
+
+			return deSerializedObject;
+
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			return null;
 		}
-		
+
 	}
 
+	// *******************************************************************************
+	// CURSOS
+
+	@SuppressWarnings("unused")
+	public static void serializeJavaObjectToDBCurso(Connection connection, String id, Object objetoGuardar)
+			throws SQLException {
+
+		// just setting the class name
+		PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_SERIALIZE_OBJECTCurso);
+		cerrarConexiones(connection);
+		// just setting the class name
+		ejecucionQuery.setString(1, id);
+		ejecucionQuery.setObject(2, objetoGuardar);
+		ejecucionQuery.executeUpdate();
+		ResultSet resultadoQuery = ejecucionQuery.getGeneratedKeys();
+		int serialized_id = -1;
+		if (resultadoQuery.next()) {
+			serialized_id = resultadoQuery.getInt(1);
+		}
+		resultadoQuery.close();
+		ejecucionQuery.close();
+		connection.close();
+
+	}
 
 	/**
-	 * Serialization and de-serialization of java object from mysql
+	 * To de-serialize a java object from database
 	 *
-	 * @throws ClassNotFoundException
 	 * @throws SQLException
 	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
-	public static void main(String args[]) throws ClassNotFoundException,
-			SQLException, IOException {
-				
-		Usuario user = new Usuario(1,2014086158,"Matamoros","Serrano","Kevin","k.andres0620@gmail.com","aaaAAA123");
-		
-		serializeJavaObjectToDB(conexionDB(),"peresultadoQueryona1",user,"k.andres0620@gmail.com");
+	public static Object deSerializeJavaObjectFromDBCurso(Connection connection, String serialized_id) {
+		try {
+			cerrarConexiones(connection);
+			PreparedStatement ejecucionQuery = connection.prepareStatement(SQL_DESERIALIZE_OBJECTCurso);
+			ejecucionQuery.setString(1, serialized_id);
+			ResultSet resultadoQuery = ejecucionQuery.executeQuery();
+			resultadoQuery.next();
 
-		
-		// de-serializing java object from mysql database
-		Usuario objFromDatabase = (Usuario) deSerializeJavaObjectFromDB(conexionDB(),"k.andres0620@gmail.com");
-		System.out.println(objFromDatabase.getNombre());
-		
-		conexionDB().close();
+			byte[] buf = resultadoQuery.getBytes(1);
+			ObjectInputStream objectIn = null;
+			if (buf != null)
+				objectIn = new ObjectInputStream(new ByteArrayInputStream(buf));
+			Object deSerializedObject = objectIn.readObject();
+
+			resultadoQuery.close();
+			ejecucionQuery.close();
+			connection.close();
+			return deSerializedObject;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+
 	}
+
+	public static void cerrarConexiones(Connection connection) {
+
+		PreparedStatement ejecucionQuery;
+		try {
+			ejecucionQuery = connection.prepareStatement(SQL_CLOSE_CONNECTION);
+			ejecucionQuery.executeQuery();
+			ejecucionQuery.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
